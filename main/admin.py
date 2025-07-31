43abe304106dc40331f42c60133a50178b59a827
rohanshakya1316 <romanrohanreignsshakya@gmail.com>
2025-07-31 19:53:42 +0545

    Python Django - Models Introduction

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Task
+from .models import Task, School, Student, Address, Grade, StudentProfile
 
 # Register your models here.
 admin.site.register(Task)
+admin.site.register(School)
+admin.site.register(Student)
+admin.site.register(Address)
+admin.site.register(Grade)
+admin.site.register(StudentProfile)
--- main/models.py
+++ main/models.py
@@ -8,3 +8,44 @@ class Task(models.Model):
     def __str__(self):
         return f"{self.title}"
 
+class School(models.Model):
+    name = models.CharField(max_length=300)
+    estd_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+class Address(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Addresses"
+    
+class Grade(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    room_no = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.school} | {self.name}"
+    
+class Student(models.Model):
+    school = models.ForeignKey(School, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=200)
+    roll_no = models.IntegerField()
+    address = models.ForeignKey(Address, on_delete=models.DO_NOTHING, null=True, blank=True)
+    grade = models.ForeignKey(Grade, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+    
+class StudentProfile(models.Model):
+    student = models.OneToOneField(Student, on_delete=models.CASCADE)
+    description = models.TextField()
+    photo = models.ImageField(upload_to="student_photos", null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.student}'s profile"
